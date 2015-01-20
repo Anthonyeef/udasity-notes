0d7b9d8a2ea0943c3b4d5d37a07c03451a922d44
Anthonyeef <anthonyeef@gmail.com>
2015-01-20 17:09:25 +0100

    introduce the json knowledge

--- frontend-nanodegree-resume/js/resumeBuilder.js
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -1,13 +1,24 @@
-var name = "Yifen Wu";
-var formattedName = HTMLheaderName.replace("%data%",name);
+var bio = {
+    "name" : "Yifen Wu",
+    "role"  :"Web Developer",
+    "contacts"  :{
+        "mobile":"91859524",
+        "email" :"anthonyeef@gmail.com",
+        "github":"Anthonyeef",      
+    },
+    "welcomeMessage":"An apple a day, keep the doctor away!",
+    "skills":["awesomeness","delivering things","protecting the universe"],
+    "bioPic":"images/fry.jpg"
+}
+var work ={};
+    work.position = "internship";
+    work.years = 1;
+var education = {};
+    education["name"] = "China University of Petrolium";
+    education["city"] = "Beijing";
+    education["years"]= "4"; 
+
+$("#main").append(work["position"]);
+$("#main").append(education.name);
+
  
-
-var role = "Web Developer";
-var formattedRole = HTMLheaderRole.replace("%data%",role);
-
-var skills = ["awesome", "JS", "HTML","C++"];
-
-$("#header").prepend(formattedName);
-$("#header").prepend(formattedRole);
-
-$("#main").append(skills);
