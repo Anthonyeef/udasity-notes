1616f4e19e31e2bdba871766efe97217f14f9e21
Anthonyeef <anthonyeef@gmail.com>
2015-01-20 21:46:07 +0100

    ready for the last lecture.

--- frontend-nanodegree-resume/js/resumeBuilder.js
+++ frontend-nanodegree-resume/js/resumeBuilder.js
@@ -4,21 +4,69 @@ var bio = {
     "contacts"  :{
         "mobile":"91859524",
         "email" :"anthonyeef@gmail.com",
-        "github":"Anthonyeef",      
+        "github":"Anthonyeef",
+        "twitter":"@anthonyeef",
+        "location":"Beijing"     
     },
     "welcomeMessage":"An apple a day, keep the doctor away!",
     "skills":["awesomeness","delivering things","protecting the universe"],
     "bioPic":"images/fry.jpg"
 }
-var work ={};
-    work.position = "internship";
-    work.years = 1;
-var education = {};
-    education["name"] = "China University of Petrolium";
-    education["city"] = "Beijing";
-    education["years"]= "4"; 
+var education = {
+    "schools":[
+    {
+       "name":"China University of Petrolium",
+       "city":"Beijing",
+       "degree":"Exchange",
+       "majors":["Mechanical engineering"],
+       "dates":2012,
+       "url":"http://anthonyeef.github.io"
+    },
+    {
+        "name":"Denmark Technical University",
+        "city":"Lyngby, Denmark",
+        "degree":"BA",
+        "majors":["Mechanical engineering"],
+        "dates":2014,
+        "url":"anthonyeef.github.io"
+          }
+    ],
+    "onlineCourses":[
+    {
+        "title":"Javascript crash",
+        "school":"udasity",
+        "dates":2014,
+        "url":"https://www.udacity.com/course/viewer#!/c-ud804/l-1930528550/e-1935058561/m-1952638584"
+    }
+    ]
+}
 
-$("#main").append(work["position"]);
-$("#main").append(education.name);
+var work = {
+    "jobs":[
+    {
+        "employer":"plant express",
+        "title":"deliver boy",
+        "dates":"January 3000 - Future",
+        "description":"Here is just an example for the work experience."
 
+    },
+    {
+        "employer":"Panucci's pizza",
+        "title":"boss",
+        "dates":"1998-December",
+        "description":"Here is another example about the description"
+    }
+    ]
+}
+
+var projects = {
+    "projects":[
+    {
+        "title":"Sample Project 1",
+        "dates":"2014",
+        "description":"This is another description for the project 1"
+    }
+    ]
+
+}
  
